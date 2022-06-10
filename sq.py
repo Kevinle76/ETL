16ae5a04f66de882002aa175ab72f3cbf4acd01a
lukeCrabtree653 <100876006+lukeCrabtree653@users.noreply.github.com>
2022-06-10 13:42:40 -0400

    adding the version of the code i worked on
    includes basic code and sql code to create table

--- sq.py
+++ sq.py
@@ -1,28 +1,29 @@
-CREATE TABLE DIVERS (
+ CREATE TABLE unemp (
+ 	Location VARCHAR(255),
+	State VARCHAR(255), 
+ 	County	VARCHAR(255),
+ 	Rate FLOAT 
+ 	);
 	
+ CREATE TABLE inco (
+ 		Location VARCHAR(255),
+	    State VARCHAR(255) ,
+ 		County VARCHAR(255), 
+ 		Population INT,
+ 		Median_household_income	FLOAT
+ );	
+
+CREATE TABLE DIVERS (
+	Location VARCHAR(255),
 	State VARCHAR(255),
 	County VARCHAR(255), 
-	FOREIGN KEY (County) REFERENCES unemp(County),
   	Diver_index FLOAT,
  	Black FLOAT,
  	American FLOAT,
 	Asian FLOAT,
 	Hawaian FLOAT,
-	Two_more_races FLOAT,
+	Two_or_more_races FLOAT,
 	Hispanic FLOAT,
  	White FLOAT
  );
 
-	
--- CREATE TABLE unemp (
--- 	State VARCHAR(255) , 
--- 	County	VARCHAR(255) PRIMARY KEY,
--- 	Rate FLOAT 
--- 	);
--- CREATE TABLE inco (
--- 		State VARCHAR(255) ,
--- 		County VARCHAR(255), 
--- 		FOREIGN KEY (County) REFERENCES unemp(County),
--- 		Population INT,
--- 		Median_household_income	FLOAT
--- );
